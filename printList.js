a8b50edae0a5d1809c83b15987b5537da85332d4
Arevik <a.iskandaryan@gmail.com>
2020-07-18 02:00:40 +0400

    created function which print all list values in object

---
+++ printList.js
@@ -0,0 +1,21 @@
+let list = {
+    value: 1,
+    next: {
+        value: 2,
+        next: {
+            value: 3,
+            next: {
+                value: 4,
+                next: null,
+            },
+        },
+    },
+};
+
+function printList(list) {
+    console.log(list.value);
+    if (list.next !== null) {
+        return printList(list.next);
+    }
+}
+console.log(printList(list));
